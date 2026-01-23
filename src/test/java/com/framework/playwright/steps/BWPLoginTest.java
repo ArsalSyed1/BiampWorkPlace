45bd2cc25b164dd073c8d5aa08c8f79053e08b95
Arsal Syed <arsal.syed@teo-intl.com>
2026-01-23 16:27:58 +0500

    Added traces

--- src/test/java/com/framework/playwright/steps/BWPLoginTest.java
+++ src/test/java/com/framework/playwright/steps/BWPLoginTest.java
@@ -4,19 +4,20 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.Cookie;
 import com.microsoft.playwright.options.SameSiteAttribute;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 import static com.microsoft.playwright.options.WaitForSelectorState.VISIBLE;
 
 public class BWPLoginTest {
 
-    Playwright playwright = Playwright.create();
-    Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
-    BrowserContext context = browser.newContext();
-    Page page = context.newPage();
+
+    Playwright playwright;
+    Browser browser;
+    BrowserContext context;
+    Page page;
 
     Cookie vercelCookie = new Cookie("_vercel_jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwYXNzd29yZC1wcm90ZWN0aW9uIiwiaGFzaCI6IjI3Zjc4ODM4MmNhZTAyNjk0YzU5YzRjNjFmYjNjYzU2YmRlZjQxMzBkZTZhYjk5ODFmYjIzYmFlYjM5YzcyOWEiLCJhdWQiOiJzdGFnZS53b3JrcGxhY2UuYmlhbXAuYXBwIiwiaWF0IjoxNzY5MDc4Mjg3fQ.LbiyOhjRsQZOtHWzrTHIMQ1WT92ZRFew0R-yQju4Dqw")
             .setDomain("stage.workplace.biamp.app")
@@ -25,9 +26,26 @@ public class BWPLoginTest {
             .setSecure(true)
             .setSameSite(SameSiteAttribute.LAX);
 
+    @BeforeEach
+    void setupTrace(){
+
+        playwright= Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext();
+        context.addCookies(List.of(vercelCookie));
+        context.tracing().start(
+                new Tracing.StartOptions()
+                        .setScreenshots(true)
+                        .setSnapshots(true)
+                        .setSources(true) );
+        page = context.newPage();
+    }
+
+
+
     @Test
     void login() throws InterruptedException {
-        context.addCookies(List.of(vercelCookie));
+        // context.addCookies(List.of(vercelCookie));
         page.navigate("https://stage.workplace.biamp.app");
         page.getByText("Sign in or Register").click();
         page.locator("#loginName").fill("arsal.syed@biamp.com");
@@ -42,4 +60,24 @@ public class BWPLoginTest {
         System.out.println(count);
         Assertions.assertEquals(1, count);
     }
+
+
+    @AfterEach
+    void recordTraces(TestInfo testInfo){
+
+        String traceName = testInfo.getDisplayName().replace(" ", "-") .replaceAll("[\\\\/:*?\"<>|()]", "") // remove illegal Windows chars
+                .toLowerCase();
+
+        context.tracing().stop(
+                new Tracing.StopOptions()
+                        .setPath(Paths.get("trace-"+traceName+".zip")) // Will save with display name of test
+        );
+
+        context.close();
+        browser.close();
+        playwright.close();
+    }
+
+
+
 }
