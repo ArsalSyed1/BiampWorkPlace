9b440b723026cbe7c1021e96ff58f651823fa599
Arsal Syed <arsal.syed@teo-intl.com>
2026-01-28 17:18:01 +0500

    base test and page classes implemented

--- src/test/java/biamp/framework/biampworkplace/base/basePage.java
+++ src/test/java/biamp/framework/biampworkplace/base/basePage.java
@@ -1,4 +1,71 @@
 package biamp.framework.biampworkplace.base;
 
+import com.microsoft.playwright.Download;
+import com.microsoft.playwright.Page;
+
+import java.nio.file.Paths;
+
 public class basePage {
+
+    private final Page page;
+    // Constructor
+    public basePage(Page page) {
+        this.page = page;
+    }
+
+
+    public void navigateTo(String url) {
+        page.navigate(url);
+    }
+
+    public void click(String selector) {
+        page.locator(selector).click();
+    }
+
+    public void type(String selector, String text) {
+        page.locator(selector).fill(text);
+    }
+
+    public String getText(String selector) {
+        return page.locator(selector).innerText();
+    }
+
+    public boolean isVisible(String selector) {
+        return page.locator(selector).isVisible();
+    }
+
+    public void hover(String selector) {
+        page.locator(selector).hover();
+    }
+
+    public void doubleClick(String selector) {
+        page.locator(selector).dblclick();
+    }
+
+    public void selectOption(String selector, String value) {
+        page.locator(selector).selectOption(value);
+    }
+
+    public boolean containsText(String selector, String text) {
+        return page.locator(selector).innerText().contains(text);
+    }
+
+    public void clearField(String selector) {
+        page.locator(selector).fill("");
+    }
+
+    // Method to upload a file
+    public void uploadFile(String selector, String filePath) {
+        page.locator(selector).setInputFiles(Paths.get(filePath));
+    }
+
+    // Method to download a file
+    public void downloadFile(String downloadUrl) {
+        Download download = page.waitForDownload(() -> {
+            page.click("#downloadBtn");
+        });
+        download.saveAs(Paths.get("downloads/report.pdf"));
+    }
+
+
 }
--- src/test/java/biamp/framework/biampworkplace/base/baseTest.java
+++ src/test/java/biamp/framework/biampworkplace/base/baseTest.java
@@ -1,10 +1,70 @@
 package biamp.framework.biampworkplace.base;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.Cookie;
+import com.microsoft.playwright.options.SameSiteAttribute;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+
+import java.nio.file.Paths;
+
+import static com.framework.playwright.utilities.auth_sessionUtils.reuseSession;
+
 public class baseTest {
 
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext context;
+    protected Page page;
+
+    static Cookie vercelCookie = new Cookie("_vercel_jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwYXNzd29yZC1wcm90ZWN0aW9uIiwiaGFzaCI6IjI3Zjc4ODM4MmNhZTAyNjk0YzU5YzRjNjFmYjNjYzU2YmRlZjQxMzBkZTZhYjk5ODFmYjIzYmFlYjM5YzcyOWEiLCJhdWQiOiJzdGFnZS53b3JrcGxhY2UuYmlhbXAuYXBwIiwiaWF0IjoxNzY5MDc4Mjg3fQ.LbiyOhjRsQZOtHWzrTHIMQ1WT92ZRFew0R-yQju4Dqw")
+            .setDomain("stage.workplace.biamp.app")
+            .setPath("/")
+            .setHttpOnly(true)
+            .setSecure(true)
+            .setSameSite(SameSiteAttribute.LAX);
+
+    public baseTest(Page page) {
+        this.page = page;
+    }
+
+    @BeforeAll
+    public static void setup() {
 
 
+    }
 
+    @BeforeEach
+    public void beforeEach() {
+        playwright= Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context =reuseSession(browser);
+        context.tracing().start(
+                new Tracing.StartOptions()
+                        .setScreenshots(true)
+                        .setSnapshots(true)
+                        .setSources(true) );
+
+        page = context.newPage();
+
+    }
+
+    @AfterEach
+    public void afterEach(TestInfo testInfo) {
+        String traceName = testInfo.getDisplayName().replace(" ", "-") .replaceAll("[\\\\/:*?\"<>|()]", "") // remove illegal Windows chars
+                .toLowerCase();
+
+        context.tracing().stop(
+                new Tracing.StopOptions()
+                        .setPath(Paths.get("src/test/java/com/framework/playwright/artifacts/" + "traces-"+traceName+".zip")) // Will save with display name of test
+        );
+
+        context.close();
+        browser.close();
+        playwright.close();
+    }
 
 
 }
